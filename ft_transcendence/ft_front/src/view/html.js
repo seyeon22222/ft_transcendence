e231a4f81f38be8e62123ce8aad0bd1146953164
김세연 <seyeon@seyeon.local>
2024-05-14 17:19:04 +0900

    Feat : make userProfile View

--- ft_transcendence/ft_front/src/view/html.js
+++ ft_transcendence/ft_front/src/view/html.js
@@ -5,10 +5,9 @@ export function profile_html() {
     </div>
     
     <form class="mb-5" id="profile_form">
+        <div id="profile-image">프로필 사진</div>
         <input type="text" name="username" class="w-full mt-2 px-4 py-2 rounded-xl" id="username_input">
         <input type="email" name="email" class="w-full mt-2 px-4 py-2 rounded-xl" id="email_input">
-        <!-- 이미지 빌드 파트 --> 
-        <input id="image">
         <h1 class="w-full mt-2 px-4 py-2 rounded-xl" id="game_status"></h1>
         <h1 class="w-full mt-2 px-4 py-2 rounded-xl" id="match_info"></h1>
         <div>
