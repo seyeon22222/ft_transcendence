0caca96766b82fc0d3d6602aff8588db181a44d7
chanwoo4267 <chanwoo4267@naver.com>
2024-05-15 16:21:39 +0900

    chatting room on progress

--- ft_transcendence/ft_front/base/router.js
+++ ft_transcendence/ft_front/base/router.js
@@ -20,22 +20,6 @@ const routes = {
     "/chat": [chat_html, chat_js],
 };
 
-// function resolveRoutes(user_location) {
-//   let render;
-//   let main_path;
-//   let hash;
-
-//   main_path = `/${user_location[0]}`;
-
-//   if (routes[main_path] && user_location.length >= 1 && user_location.length <= 2) {
-//       render = routes[`/${user_location[0]}`];
-//   } else {
-//       // 404 error handling
-//   }
-
-//   return render;
-// };
-
 const router = async() => {
 
   let render;
@@ -45,7 +29,6 @@ const router = async() => {
   const content = document.getElementById("content");
 
   let user_location = location.hash.slice(1).toLocaleLowerCase().split("/");
-  // render = resolveRoutes(user_location);
 
   main_path = `/${user_location[0]}`;
   render = routes[`/${user_location[0]}`];
@@ -54,35 +37,9 @@ const router = async() => {
   if (user_location.length >= 2) {
     hash = `/${user_location[1]}`;
     await render[1](hash);
+  } else {
+    await render[1]();
   }
-  
-  // content.innerHTML = await render[0]();
-  // for (let index = 1; index < render.length; index++) {
-  //     await render[index]();
-  // }
 };
 
 export default router;
-
-// const contentDiv = document.getElementById('content');
-
-// function router() {
-//   const path = window.location.pathname;
-//   const handler = routes[path] || renderNotFound;
-
-//   if (path === '/chatting/room/') {
-//     // 실제 room 데이터와 messages, user 데이터를 가져와서 인자로 전달해야 함
-//     const roomData = fetchRoomData(); // 서버에서 room 데이터를 가져오는 함수
-//     const messagesData = fetchMessages(); // 서버에서 messages 데이터를 가져오는 함수
-//     const userData = fetchUserData(); // 서버에서 user 데이터를 가져오는 함수
-
-//     contentDiv.innerHTML = handler(roomData, messagesData, userData);
-//   } else if (path === '/chatting/rooms/') {
-//     // 실제 rooms 데이터를 가져와서 인자로 전달해야 함
-//     const roomsData = fetchRoomsData(); // 서버에서 rooms 데이터를 가져오는 함수
-
-//     contentDiv.innerHTML = handler(roomsData);
-//   } else {
-//     contentDiv.innerHTML = handler();
-//   }
-// }
