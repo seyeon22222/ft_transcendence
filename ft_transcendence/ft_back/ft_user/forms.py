af75b02d46df87e3c1f9172f90deafa951e5b9fb
김세연 <seyeon@seyeon.local>
2024-05-15 15:07:07 +0900

    Reset

--- ft_transcendence/ft_back/ft_user/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from ft_user.models import MyUser
-
-class signForm(forms.ModelForm):
-    password = forms.CharField(label='Password', widget=forms.PasswordInput)
-
-    class Meta:
-        model = MyUser
-        fields = ['username', 'password', 'email']
--- ft_transcendence/ft_front/src/chat/app.js
+++
@@ -1,3 +0,0 @@
-export async function chat_js(hash) {
-    
-}
