77837a650c03789b76e993a6ac70833b0629f327
wwwlnyy <wwwlnyy@gmail.com>
2024-06-11 21:05:21 +0900

    multimatch

--- ft_transcendence/ft_front/src/multimatch/html.js
+++ ft_transcendence/ft_front/src/multimatch/html.js
@@ -3,143 +3,48 @@ export function multiMatch_html() {
         <div class="p-10 lg:p-20 text-center">
             <h1 class="text-3xl lg:text-6xl" id="tournament_name"></h1>
         </div>
-        <div class="theme theme-dark">
-            <div class="bracket disable-borders">
-                <div class="column one">
-                    <div class="match winner-top">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_1"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_2"></span>
-                            <span class="score">1</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                    <div class="match winner-bottom">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_3"></span>
-                            <span class="score">1</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_4"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                    <div class="match winner-top">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_5"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_6"></span>
-                            <span class="score">0</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                    <div class="match winner-top">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_7"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="Round_8_8"></span>
-                            <span class="score">1</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column two">
-                    <div class="match winner-bottom">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="quarter_final1"></span>
-                            <span class="score">1</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="quarter_final2"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                    <div class="match winner-bottom">
-                        <div class="match-top team">
-                            <span class="image"></span>
-                            <span class="name" id="quarter_final3"></span>
-                            <span class="score">1</span>
-                        </div>
-                        <div class="match-bottom team">
-                            <span class="image"></span>
-                            <span class="name" id="quarter_final4"></span>
-                            <span class="score">2</span>
-                        </div>
-                        <div class="match-lines">
-                            <div class="line one"></div>
-                            <div class="line two"></div>
-                        </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="column three">
-                    <div class="match winner-top">
+		<div class="theme theme-dark">
+			<div class="bracket disable-borders">
+				<div class="column">
+					<div class="match">
+						<div class="match-top team">
+							<span class="image"></span>
+							<span class="name" id="final1"></span>
+						</div>
+						<div class="match-bottom team">
+							<span class="image"></span>
+							<span class="name" id="final2"></span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="bracket disable-borders">
+				<div class="column">
+                    <div class="match">
                         <div class="match-top team">
                             <span class="image"></span>
                             <span class="name" id="semi_final1"></span>
-                            <span class="score">3</span>
                         </div>
                         <div class="match-bottom team">
                             <span class="image"></span>
                             <span class="name" id="semi_final2"></span>
-                            <span class="score">2</span>
                         </div>
-                        <div class="match-lines">
-                            <div class="line three"></div>
+                    </div>
+                </div>
+				<div class="column">
+					<div class="match-lines">
+						<div class="line"></div>
+					</div>
+				</div>
+				<div class="column">
+                    <div class="match">
+                        <div class="match-top team" id="semi_final3">
+                            <span class="image"></span>
+                            <span class="name" id="semi_final3"></span>
                         </div>
-                        <div class="match-lines alt">
-                            <div class="line one"></div>
+                        <div class="match-bottom team">
+                            <span class="image"></span>
+                            <span class="name" id="semi_final4"></span>
                         </div>
                     </div>        
                 </div>
